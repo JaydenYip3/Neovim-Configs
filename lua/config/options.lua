238a5a38e22198a36298cd8060ea2c305d55e428
JaydenYip3 <jy1937jy@gmail.com>
2026-01-22 00:04:21 -0500

    Fix tabspacing

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -46,7 +46,6 @@ vim.o.splitbelow = true
 
 --
 vim.o.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
 -- Preview substitutions live, as you type!
 vim.o.inccommand = 'split'
@@ -61,3 +60,9 @@ vim.o.scrolloff = 10
 -- instead raise a dialog asking if you wish to save the current file(s)
 -- See `:help 'confirm'`
 vim.o.confirm = true
+
+-- Tab size
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
