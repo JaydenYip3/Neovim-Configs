fe550476b4ae4d4758e501d15614976715a51763
JaydenYip3 <jy1937jy@gmail.com>
2026-01-30 15:16:41 -0500

    Edit nvim plugin neotree to always open neotree when a new file is opened

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -13,11 +13,31 @@ return {
   keys = {
     { '<leader>e', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
+
   opts = {
+    close_if_last_window = false, -- (this is top-level)
+
     filesystem = {
       window = {
         mappings = {
           ['<leader>e'] = 'close_window',
+
+          -- ✅ make `t` open file in new tab AND also open neo-tree in that tab
+          ['t'] = function(state)
+            -- 1) open selected node in a new tab (default behavior)
+            require('neo-tree.sources.filesystem.commands').open_tabnew(state)
+
+            -- 2) ensure neo-tree is shown in the new tab, but keep focus on the file
+            vim.schedule(function()
+              local file_win = vim.api.nvim_get_current_win()
+              require('neo-tree.command').execute {
+                action = 'show',
+                source = 'filesystem',
+                reveal = true,
+              }
+              pcall(vim.api.nvim_set_current_win, file_win)
+            end)
+          end,
         },
       },
     },
