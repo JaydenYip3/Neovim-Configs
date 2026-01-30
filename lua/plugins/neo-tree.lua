beaa5b3e924a28ee0f9923a45c1bf1b2f7626173
JaydenYip3 <jy1937jy@gmail.com>
2026-01-30 15:25:08 -0500

    Edit nvim plugin neotree to have peek functionality when pressing space

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -22,7 +22,19 @@ return {
         mappings = {
           ['<leader>e'] = 'close_window',
 
-          -- ✅ make `t` open file in new tab AND also open neo-tree in that tab
+          -- Space = preview (keeps focus in neo-tree)
+          ['<space>'] = function(state)
+            local node = state.tree:get_node()
+            if node.type == 'file' then
+              require('neo-tree.sources.filesystem.commands').toggle_preview(state, {
+                use_float = false,
+              })
+            else
+              require('neo-tree.sources.filesystem.commands').toggle_node(state)
+            end
+          end,
+
+          -- make `t` open file in new tab AND also open neo-tree in that tab
           ['t'] = function(state)
             -- 1) open selected node in a new tab (default behavior)
             require('neo-tree.sources.filesystem.commands').open_tabnew(state)
